4969d9a93ef6ddf1fb408291a7d8b2d207f0f155
LBiddiscombe <lee.biddiscombe@btinternet.com>
2017-11-02 21:15:41 +0000

    trial save file on Azure

--- api/server.js
+++ api/server.js
@@ -3,6 +3,16 @@ var fiverDB = require('../fiverData.json')
 
 http
   .createServer(function(req, res) {
+    fiverDB.lastUpdate = Date()
+
+    fs.writeFile('../fiverData.json', content, 'utf8', function(err) {
+      if (err) {
+        return console.log(err)
+      }
+
+      console.log('The file was saved!')
+    })
+
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(req.url + ' - ' + JSON.stringify(fiverDB.players, null, 2))
   })
