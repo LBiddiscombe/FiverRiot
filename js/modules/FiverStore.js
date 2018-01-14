effd58676b38cbe81102ba21c3992e6f937e630f
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-01-14 19:54:56 +0000

    refactor fiverMixin into a module

--- js/modules/FiverStore.js
+++ js/modules/FiverStore.js
@@ -1,3 +1,5 @@
+import { fiverMixin } from './fiverMixin.js'
+
 function FiverStore() {
   riot.observable(this)
 
@@ -5,42 +7,6 @@ function FiverStore() {
   var fiver = {}
   var timedSave = null
   self.swapList = []
-
-  var fiverMixin = {
-    asMoney: function(value) {
-      if (!value || value === 'NaN') {
-        return '£0.00'
-      }
-      let money = Math.round(value * Math.pow(10, 2)) / Math.pow(10, 2)
-      return '£' + money.toFixed(2)
-    },
-
-    toDecimal: function(value, decimals) {
-      if (!value) value = 0
-      val = parseFloat(value)
-      return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals)
-    },
-
-    maskMoney: function(e) {
-      var val = e.target.value.replace('.', '')
-      if (val == '') {
-        return
-      }
-
-      val = val / 100
-      e.target.value = val === 0 ? '' : this.toDecimal(val, 2).toFixed(2)
-    },
-
-    asHSL: function(hsl) {
-      let headHSL = ` hsl(${hsl[0]},${hsl[1]}%,${hsl[2]}%)`
-      let mainHSL = ` hsl(${hsl[0]},15%,92%)`
-      return { headHSL, mainHSL }
-    },
-
-    getSettings: function() {
-      return self.fiver.settings
-    }
-  }
 
   riot.mixin('fiverMixin', fiverMixin)
 
---
+++ js/modules/fiverMixin.js
@@ -0,0 +1,39 @@
+var fiverMixin = {
+  asMoney: function(value) {
+    if (!value || value === 'NaN') {
+      return '£0.00'
+    }
+    let money = Math.round(value * Math.pow(10, 2)) / Math.pow(10, 2)
+    return '£' + money.toFixed(2)
+  },
+
+  toDecimal: function(value, decimals) {
+    if (!value) value = 0
+    let val = parseFloat(value)
+    return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals)
+  },
+
+  maskMoney: function(e) {
+    var val = e.target.value.replace('.', '')
+    if (val == '') {
+      return
+    }
+
+    val = val / 100
+    e.target.value = val === 0 ? '' : this.toDecimal(val, 2).toFixed(2)
+  },
+
+  asHSL: function(hsl) {
+    let headHSL = ` hsl(${hsl[0]},${hsl[1]}%,${hsl[2]}%)`
+    let mainHSL = ` hsl(${hsl[0]},15%,92%)`
+    return { headHSL, mainHSL }
+  },
+
+  getSettings: function() {
+    //TODO: This can't be good, must be a better way to get settings and pass them around, especially now an
+    //      es6 module and the direct reference to the RiotControl store!?
+    return RiotControl._stores[0].fiver.settings
+  }
+}
+
+export { fiverMixin }
