d8258eb0c108dd0e7a1096e24921f91810cf4e42
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-01-27 17:37:53 +0000

    added Raven.js for Sentry error reporting

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 /*global RiotControl */
 import { FiverStore } from '/js/modules/FiverStore.js'
 
-var fiverStore = new FiverStore()
-RiotControl.addStore(fiverStore)
+Raven.config(
+  'https://31cfcb0d43a5430bad1d892efc85b80d@sentry.io/278692'
+).install()
+
+Raven.context(function() {
+  var fiverStore = new FiverStore()
+  RiotControl.addStore(fiverStore)
+})
